30f10bd34f574c007f0741dbc09d2a6985070ccd
MrNechaev <igor.nechaev@inbox.ru>
2024-04-03 20:49:33 +0300

    Добавлен тест на добавление пользователя

---
+++ addresbook-web-tests/addresbook-web-tests/AccountAddData.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAddressbookTests
+{
+    internal class AccountAddData
+    {
+        private string name;
+        private string lastName;
+
+        public AccountAddData(string name, string lastName)
+        {
+            this.name = name;
+            this.lastName = lastName;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+    }
+
+    
+}
