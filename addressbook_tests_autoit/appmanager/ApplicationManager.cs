bdd18b95946bc228ed8f5da8ef1144c447472ecf
MrNechaev <igor.nechaev@inbox.ru>
2024-05-17 22:20:49 +0300

    Лекция 8.1

---
+++ addressbook_tests_autoit/appmanager/ApplicationManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using AutoItX3Lib;
+
+namespace addressbook_tests_autoit
+{
+    public class ApplicationManager
+    {
+        public static string WINTITLE = "Free Address Book";
+        private AutoItX3 aux;
+        private GroupHelper groupHelper;
+        public ApplicationManager() 
+        {
+            groupHelper = new GroupHelper(this);
+            aux = new AutoItX3();
+            aux.Run(@"F:\FreeAddressBookPortable\AddressBook.exe", "", aux.SW_SHOW);
+            aux.WinActivate(WINTITLE);
+            aux.WinWait(WINTITLE);
+            aux.WinWaitActive(WINTITLE);
+
+        }
+
+        public void Stop()
+        {
+            aux.ControlClick(WINTITLE, "", "WindowsForms10.BUTTON.app.0.2c908d510");
+        }
+
+        public AutoItX3 Aux
+        {
+            get { return aux; }
+        }
+
+        public GroupHelper Groups
+        {
+            get
+            {
+                return groupHelper;
+            }
+        }
+    }
+}
